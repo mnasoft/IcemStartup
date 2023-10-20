d7aad85e7bb9ad0e7c3fe104282f77f3aa3abb6e
Mykola Matvyeyev <mnasoft@gmail.com>
2023-10-20 22:21:07 +0300

    Fri Oct 20 19:21:07 UTC 2023

---
+++ IcemRequire.tcl
@@ -0,0 +1,8 @@
+# source $env(TCL_HOME)/IcemStartup/IcemRequire.tcl
+mess "\nStart Loading IcemRequire.tcl ...\n"
+
+package require MnasIcemUtils
+package require N70_base
+
+mess "\nIcemRequire.tcl - LOADED\n"
+
--- IcemStartup.tcl
+++ IcemStartup.tcl
@@ -6,6 +6,7 @@ global Tcl_User_Root
 set Tcl_User_Root $env(TCL_HOME)
 
 ic_gui_set eval source $Tcl_User_Root/PkgLoader/PkgLoader.tcl
+ic_gui_set eval source $Tcl_User_Root/IcemStartup/IcemRequire.tcl
 
 mess "\nIcemStartup.tcl - LOADED\n"
 
