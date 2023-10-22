0829a4aa618e060e01e0189b014990da17a98353
Mykola Matvyeyev <mnasoft@gmail.com>
2023-10-22 20:19:02 +0300

    Sun Oct 22 17:19:02 UTC 2023

--- IcemRequire.tcl
+++ IcemRequire.tcl
@@ -3,9 +3,14 @@
 mmsg_start
 
 addRelToPkgLoaderDirs ICEM
+addToPkgLoaderDirs MnasTkUtils
+
 createPkgIndex
 
 package require MnasIcemUtils
 package require N70_base
+package require MnasTkUtils
+
+menu_WindowNavigator
 
 mmsg_finish
