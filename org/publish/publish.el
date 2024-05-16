ac784d7e4747c5d12995c61aa78afff1d1a304df
Mykola Matvyeyev <mnasoft@gmail.com>
2024-05-16 11:56:15 +0300

    Thu May 16 08:56:15 UTC 2024

--- org/publish/publish.el
+++ org/publish/publish.el
@@ -7,42 +7,20 @@
     (setq prefix local-prefix))
    (t (setq prefix n133906-prefix)))
 ;;;;
-  (setq prj-root (get-ancestor-directory-name
-                  (get-relative-path
-                   (buffer-file-name)) 3)
-        html-head "<link rel=\"stylesheet\" href=\"../other/mystyle.css\" type=\"text/css\"/>")
+  (setq prj-root
+        (get-ancestor-directory-name (get-relative-path (buffer-file-name)) 3))
 
   (setq org-publish-project-alist
-        `(("pub-org"
-           :base-directory ,(concat "~/" prj-root "/org/publish/")
-           :base-extension "org"
-           :publishing-directory ,(concat prefix prj-root "/org/publish/")
-           :publishing-function org-html-publish-to-html
-           :headline-levels 3
-           :section-numbers nil
-           :with-toc nil
-           :html-head ,html-head
-           :html-preamble t)
-          ("main-org"
-           :base-directory ,(concat "~/" prj-root "/org/")
-           :base-extension "org"
-           :publishing-directory ,(concat prefix prj-root "/org/")
-           :publishing-function org-html-publish-to-html
-           :exclude "ToDo.org" ;; regexp
-           :headline-levels 3
-           :section-numbers nil
-           :with-toc nil
-           :html-head ,html-head
-           :html-preamble t)
-          ("images"
-           :base-directory ,(concat "~/" prj-root "/images/")
-           :base-extension "png"
-           :publishing-directory ,(concat prefix prj-root "/images/")
-           :publishing-function org-publish-attachment)
-          ("website" :components ("pub-org" "main-org"  "images"))))
-;;;;
+        `(
+          ,(org-pub-list "pub"          "org/publish")
+          ,(org-pub-list "main"         "org" :with-toc t)
+;;;;          
+          ,(org-att-list "images" "png" "images" :recursive t)
+          ,(org-att-list "pub"    "el"  "org/publish")))
+  (org-web-list))
+
+(progn
   (require 'ox-publish)
   (setq org-publish-use-timestamps-flag nil)
   (setq org-confirm-babel-evaluate nil) ; выполнение всех блоков кода без подтверждения
   (org-publish-project "website"))
-
